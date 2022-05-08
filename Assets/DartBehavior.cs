16c372f07509214ef9bc76d75d13f7de5b5c5c9e
Kyryl Trihub <kirill.trigub@gmail.com>
2022-05-08 15:58:27 +0200

    smth broke down

--- Assets/DartBehavior.cs
+++ Assets/DartBehavior.cs
@@ -14,36 +14,37 @@ public class DartBehavior : MonoBehaviour
     public Collider tipCollider;
     void Update()
     {
-        DartRaycast();
-        DartStop();
+        //DartRaycast();
+        //DartStop();
     }
-    public void DartRaycast()
-    {
-        if (DartHitSmth)
-        {
-            return;
-        }
+    //public void DartRaycast()
+    //{
+    //    if (DartHitSmth)
+    //    {
+    //        return;
+    //    }
 
-        Physics.Raycast(dartRayCastOrigin.transform.position, dartRayCastEnd.transform.position, out RaycastHit dartHit, dartLayerMask);
+    //    Physics.Raycast(dartRayCastOrigin.transform.position, dartRayCastEnd.transform.position, out RaycastHit dartHit, dartLayerMask);
             
-        if(!dartHit.collider)
-        {
-            return;
-        }
+    //    if(!dartHit.collider)
+    //    {
+    //        return;
+    //    }
 
-        DartHitSmth = true;
-        Debug.Log(dartHit.collider.gameObject.name);
-    }
+    //    DartHitSmth = true;
+    //    Debug.Log(dartHit.collider.gameObject.name);
+    //}
 
-    public void DartStop()
-    {
-        if (!DartHitSmth)
-        {
-            return;
-        }
+    //public void DartStop()
+    //{
+    //    if (!DartHitSmth)
+    //    {
+    //        return;
+    //    }
 
-        dartRb.useGravity = false;
-        dartRb.velocity = new Vector3(0, 0, 0);
-        tipCollider.enabled = false;
-    }
+    //    dartRb.useGravity = false;
+    //    dartRb.isKinematic = true;
+    //    dartRb.velocity = new Vector3(0, 0, 0);
+    //    tipCollider.enabled = false;
+    //}
 }
